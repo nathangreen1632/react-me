4c72403c7f9f8205db45b3b356da41b352d60b94
Nathan Green <ngreen1632@gmail.com>
2025-01-10 13:30:10 -0600

    Updates styling for footer buttons

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -13,28 +13,35 @@ const App = () => {
       <header>
         <h1>Nathan Green</h1>
         <nav>
-          <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}><button>About Me</button></NavLink>
-          <br/>
-          <NavLink to="/portfolio" className={({ isActive }) => (isActive ? 'active' : '')}><button>Portfolio</button></NavLink>
-          <br/>
-          <NavLink to="/contact" className={({ isActive }) => (isActive ? 'active' : '')}><button>Contact</button></NavLink>
-          <br/>
-          <NavLink to="/resume" className={({ isActive }) => (isActive ? 'active' : '')}><button>Resume</button></NavLink>
+          <NavLink to="/" className={({isActive}) => (isActive ? 'active' : '')}>
+            <button>About Me</button>
+          </NavLink>
+          <NavLink to="/portfolio" className={({isActive}) => (isActive ? 'active' : '')}>
+            <button>Portfolio</button>
+          </NavLink>
+          <NavLink to="/contact" className={({isActive}) => (isActive ? 'active' : '')}>
+            <button>Contact</button>
+          </NavLink>
+          <NavLink to="/resume" className={({isActive}) => (isActive ? 'active' : '')}>
+            <button>Resume</button>
+          </NavLink>
         </nav>
+
       </header>
       <main>
         <Routes>
-          <Route path="/" element={<AboutMe />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/resume" element={<Resume />} />
+          <Route path="/" element={<AboutMe/>}/>
+          <Route path="/portfolio" element={<Portfolio/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/resume" element={<Resume/>}/>
         </Routes>
       </main>
       <footer>
         <p>© {new Date().getFullYear()} Nathan Green</p>
         <br/>
         <div>
-          <a href="https://github.com/nathangreen1632" target="_blank" rel="noopener noreferrer"><button>GitHub</button></a>
+          <a href="https://github.com/nathangreen1632" target="_blank" rel="noopener noreferrer">
+            <button>GitHub</button></a>
           <a href="https://www.linkedin.com/in/jgreen1632" target="_blank" rel="noopener noreferrer"><button>LinkedIn</button></a>
           <a href="https://stackoverflow.com/users/27279774/nathan" target="_blank" rel="noopener noreferrer"><button>Stack Overflow</button></a>
         </div>
