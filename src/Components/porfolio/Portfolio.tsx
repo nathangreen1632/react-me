15183540920178252c76fcf41ddb5683cd2c17b5
Nathan Green <ngreen1632@gmail.com>
2025-01-10 23:41:27 -0600

    Adds portfolio items to page

--- src/Components/porfolio/Portfolio.tsx
+++ src/Components/porfolio/Portfolio.tsx
@@ -3,9 +3,7 @@ const Portfolio = () => {
     { title: 'Weather Dashboard', link: 'https://weather-dashboard-c4pa.onrender.com/', repo: 'https://github.com/nathangreen1632/weather-dashboard' },
     { title: 'Recipe Planner (takes a minute to render)', link: 'https://recipe-planner-1632.netlify.app/', repo: 'https://github.com/nathangreen1632/recipe-planner' },
     { title: 'Payroll Tracker', link: 'https://payroll-tracker.netlify.app/', repo: 'https://github.com/nathangreen1632/employee-payroll-tracker' },
-    { title: 'Tech Blog', link: 'https://tech-blog-1632.herokuapp.com/', repo: 'https://github.com/nathangreen1632/tech-blog' },
-    { title: 'Note Taker', link: 'https://note-taker-1632.herokuapp.com/', repo: ''},
-    { title: 'Budget Tracker', link: 'https://budget-tracker-1632.herokuapp.com/', repo: ''},
+    { title: 'Tech Blog', link: 'https://toggle-blog.netlify.app', repo: 'https://github.com/nathangreen1632/toggle-blog' },
     // Add more projects
   ];
 
