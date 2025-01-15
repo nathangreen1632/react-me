a8274fecf56f37fa68ac2a135bc03cf7683f284d
Nathan Green <ngreen1632@gmail.com>
2025-01-15 10:41:07 -0600

    Enhances styles.css. Updates Resume.tsx to accept enhanced styles.css

--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -3,7 +3,7 @@ const Resume = () => {
     <section>
       <h2 className="project-card">Resume</h2>
       <a href="../assets/NathanGreen_Resume.pdf" download><button>Download Resume</button></a>
-      <h3>Proficiencies</h3>
+      <h2 className="project-card">Proficiencies</h2>
       <ul className="project-card">
         <li>HTML</li>
         <li>CSS</li>
