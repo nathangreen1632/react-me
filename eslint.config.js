3dd0a4f8911cc698e8a79163a0b4a0251c122a13
Nathan Green <ngreen1632@gmail.com>
2025-01-16 19:24:27 -0600

    Updates eslint.config.js. Updates package.json & package-lock.json with new devDependencies. Adds .prettierrc. Adds _redirects to overcome lack of routing with Netlify.

--- eslint.config.js
+++ eslint.config.js
@@ -3,26 +3,30 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+      extends: [
+          js.configs.recommended,
+          ...tseslint.configs.recommended,
+          'prettier'
+      ],
+
+      files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+      plugins: {
+          prettier: prettierPlugin,
+          'react-hooks': reactHooks,
+          'react-refresh': reactRefresh,
+      },
+
+      rules: {
+          'prettier/prettier': 'error',
+      },
+    }
 )
