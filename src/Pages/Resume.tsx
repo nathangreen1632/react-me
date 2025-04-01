3f38de67256b318b81ad84ff9fb7311bb08f1ccf
Nathan Green <ngreen1632@gmail.com>
2025-03-31 22:52:41 -0500

    Update Resume page styling and download functionality
    Refactored the Resume component to include responsive styling with dark mode support. Updated the resume file format from PDF to DOCX and improved the download button's design and interactivity. Enhanced layout and visual hierarchy for better user experience.

--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -1,24 +1,32 @@
 const Resume = () => {
   return (
-    <section>
-      <div>
-      <h2>Resume</h2>
+    <section className="bg-[#9CAEA9] dark:bg-black text-[#38302E] dark:text-[#ccdad1] px-6 py-6 flex justify-center items-center min-h-[calc(100vh-350px)]">
+      <div className="text-center">
+        <h2 className="text-3xl font-bold mb-4">Resume</h2>
+        <a
+          href="/Jonathan_Green_Resume.docx"
+          download="Jonathan_Green_Resume.docx"
+        >
+          <button className="px-4 py-2 mb-6 text-[#ccdad1] dark:text-orange-400 bg-black dark:bg-[#646cff] rounded text-base font-medium hover:outline hover:outline-2 hover:outline-[#646cff] hover:rounded-[30px] transition-all">
+            Download My Resume
+          </button>
+        </a>
+
+        <h2 className="text-2xl font-semibold mb-4">Proficiencies</h2>
+        <ul className="flex sm:grid-cols-3 flex-col gap-3 text-base font-medium">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>TypeScript</li>
+          <li>Python</li>
+          <li>Express</li>
+          <li>Vite</li>
+          <li>Node.js</li>
+          <li>PostgreSQL</li>
+          <li>React</li>
+          <li>CLI Apps</li>
+        </ul>
       </div>
-      <a href="public/NathanGreen_Resume.pdf" download="NathanGreen_Resume.pdf"><button>Download My Resume</button></a>
-      <h2 className="proficiencies">Proficiencies</h2>
-      <ul className="proficiencies">
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>TypeScript</li>
-        <li>Python</li>
-        <li>Express</li>
-        <li>Vite</li>
-        <li>Node.js</li>
-        <li>PostgreSQL</li>
-        <li>React</li>
-        <li>CLI Apps</li>
-      </ul>
     </section>
   );
 };
